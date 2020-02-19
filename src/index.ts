88087bfc864b78b506fde2afdb721728fd24a685
Yaopeng <yaopeng.liu@mail.utoronto.ca>
2020-02-19 15:01:27 -0500

    updated the codebase, for future scalability

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
-import insertionSort from './sorting/insertion-sort';
+import Sortings from './sortings';
+
+// interface of App instance initial params
+interface Parameters {
+    // temporarily assume all sorting methods use the same types of parameters
+    sort: Array<Number | String>
+};
+
+class App {
+    // Declaration
+    public param: Parameters;
+    // Constructor
+    constructor(param?: Parameters){
+        this.param = param;
+    }
+    // Preprocess, allow this.param as default param for methods
+    private preprocess = () : typeof Sortings => {
+        let result = Sortings;
+        for(let key in result){
+            const func = result[key];
+            result[key] = value => console.log(func(value || this.param.sort))
+        }
+        return result;
+    }
+    // expose all sorting methods
+    public sort = this.preprocess()
+}
+
+const app = new App({
+    sort: [3, 4, 7, 2, 5, 1, 6]
+});
 
 ////////////////////////////////////////////////////////////////////////////
 /// Test
 
 const array = [5, 2, 4, 6, 1, 3];
-console.log(insertionSort(array));
+app.sort.insertionSort();
+app.sort.insertionSort(array);
